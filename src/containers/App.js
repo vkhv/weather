53a3307719649ea562dfcda52f4dba6bcfec5bde
Владислав А. Хвостов <khvostov@yamoney.ru>
2017-05-06 16:50:27 +0300

    Добавить вывод давления и средней влажности воздуха

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,36 +3,35 @@ import { connect  } from 'react-redux'
 import './App.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Input from '../components/Input';
-import { combineReducers, bindActionCreators  } from 'redux';
-import { Provider  } from 'react-redux';
+import { bindActionCreators  } from 'redux';
 import * as pageActions from '../actions/WeatherActions';
 import CircularProgress from 'material-ui/CircularProgress';
 
 
 class App extends Component {
   render() {
-		  console.log(this.props.weatherApiAnswer);
 	return (
-		<div>
-
-				<MuiThemeProvider>
-					<div>
-						<Input setWeather={this.props.pageActions.setWeather} />
-
-						// Добавить спиннер на загрузку
-						{
-							this.props.weatherApiAnswer && this.props.weatherApiAnswer.fetch &&
-								<CircularProgress />
-						}
-
-						// Отрисовать данные
-						{
-							this.props.weatherApiAnswer && this.props.weatherApiAnswer.data &&
-								this.props.weatherApiAnswer.data[0].temp
-						}
-					</div>
-				</MuiThemeProvider>
-		</div>
+		<MuiThemeProvider>
+			<div>
+				<Input setWeather={this.props.pageActions.setWeather} />
+				{
+					// Добавить спиннер на загрузку
+					this.props.weatherApiAnswer && this.props.weatherApiAnswer.fetch &&
+						<CircularProgress />
+				}
+				{
+					// Отрисовать данные
+					this.props.weatherApiAnswer && this.props.weatherApiAnswer.data &&
+						<div>
+							<span>Temperature - {this.props.weatherApiAnswer.data[0].temp}</span>
+							<br />
+							<span>Preasure - {this.props.weatherApiAnswer.data[0].pres}</span>
+							<br />
+							<span>Relative Humidity - {this.props.weatherApiAnswer.data[0].rh}</span>
+						</div>
+				}
+			</div>
+		</MuiThemeProvider>
 	);
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,15 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
 import './index.css';
-import { createStore  } from 'redux';
 import { Provider  } from 'react-redux';
 import configureStore from './store/configureStore'
 
 const store = configureStore();
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
 );
